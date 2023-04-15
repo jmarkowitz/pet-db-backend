36fe0f3bf90efcf12f27646607d5987e8aac97b4
CiciLing <cicilingg0502@gmail.com>
2023-04-15 16:06:23 -0400

    pet get id

--- flask-app/src/borrowers/borrowers.py
+++ flask-app/src/borrowers/borrowers.py
@@ -3,3 +3,28 @@ import json
 from src import db
 
 borrowers = Blueprint('borrowers', __name__)
+
+@borrowers.route('/pet', methods=['GET'])
+def get_pet():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # use cursor to query the database for a list of products
+    cursor.execute('SELECT pet_id FROM Pet;')
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
