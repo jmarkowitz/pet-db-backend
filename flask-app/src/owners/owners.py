55da4a08744c1e2d4f42e7d8ea8b027d3ca6934e
mahekagg <105991553+mahekagg@users.noreply.github.com>
2023-04-17 22:54:48 -0400

    Mahek1 (#5)
    * first
    
    * something
    
    * update

--- flask-app/src/owners/owners.py
+++ flask-app/src/owners/owners.py
@@ -23,6 +23,7 @@ def get_pet_breeds(speciesID):
 
 
 
+
 @owners.route("/events", methods=['GET'])
 def get_events():
     cursor = db.get_db().cursor()
@@ -68,12 +69,14 @@ def add_new_event():
 def update_event():
     the_data = request.json
     current_app.logger.info(the_data)
-    description = the_data['description']
-    event_date = the_data['event_date']
-    city = the_data['city']
-    state = the_data['state']
-    zip_code = the_data['zip']
-    event_id = the_data['event_id']
+
+    description = the_data['description_update']
+    event_date = the_data['event_date_update']
+    city = the_data['city_update']
+    state = the_data['state_update']
+    zip_code = the_data['zip_update']
+    event_id = the_data['event_id_update']
+
 
     query = 'UPDATE Event SET description = %s, event_date = %s, city = %s, state = %s, zip = %s WHERE event_id = %s'
 
@@ -100,8 +103,6 @@ def delete_event():
 
     # commit the transaction to make the changes permanent
     db.get_db().commit()
-
-    # return a success message
 
     return "Success!"
 
