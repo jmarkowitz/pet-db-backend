acc12a407c5fa17a5fcdb0c48e80456c9df28c6b
jmarkowitz <markowitzjonathan@me.com>
2023-04-17 20:03:15 -0400

    added insert statements for OwnerPets and updated routes in owners.py

--- flask-app/src/owners/owners.py
+++ flask-app/src/owners/owners.py
@@ -3,3 +3,19 @@ import json
 from src import db
 
 owners = Blueprint('owners', __name__)
+
+@owners.route('/petTypes', methods=['GET'])
+def get_pet_types():
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT species_name FROM PetSpecies')
+    column_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+    return jsonify(json_data)
+
+@owners.route('/petSpecificBreeds', methods=['GET'])
+def get_pet_breeds(speciesID):
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT breed_name FROM PetBreeds WHERE species_id = %s', (speciesID,))
